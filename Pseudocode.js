bc2598ffb067113551e3199ee5683fdfa257099d
Moses <hamzamp13@gmail.com>
2026-05-06 15:38:42 +0200

    done

---
+++ Pseudocode.js
@@ -0,0 +1,16 @@
+studentName = input "Moses: "
+score = input "75: " 
+studentName = "Peace"
+score = 75
+
+
+if score >= 75 and score < 90 then                                          
+
+if score < 50 then
+    print "Fail"
+else if score >= 50 and score < 75 then
+    print "Pass"
+else
+    print "Excellent"
+
+console.log(studentName + " scored " + score);
